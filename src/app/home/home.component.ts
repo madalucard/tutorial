8c8a042e56d9879c256ad765384ffd2fa120fe34
Martin Lapihuska <madalucard@gmail.com>
2024-06-05 10:12:50 +0200

    Product component, global styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,33 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product, Products } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productService: ProductService) {}
 
+  products: Product[] = [];
+
   ngOnInit() {
     this.productService
-      .getProducts('http://localhost:3000/clothes', { page: 1, perPage: 5 })
+      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
       .subscribe((products: Products) => {
+        this.products = products.items;
+
         console.log(products.items);
       });
+  }
+
+  onProductOutput(product: Product) {
+    console.log('Output', product);
   }
 }
