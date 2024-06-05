426d59bc3076b11e76c0197d80261c4fd3d15d5f
Martin Lapihuska <madalucard@gmail.com>
2024-06-05 10:39:51 +0200

    Paginations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,18 +16,27 @@ export class HomeComponent {
   constructor(private productService: ProductService) {}
 
   products: Product[] = [];
+  totalRecords: number = 0;
+  rows: number = 5;
 
   ngOnInit() {
+    this.fetchProducts(0, this.rows);
+  }
+
+  fetchProducts(page: number, perPage: number) {
     this.productService
-      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .getProducts('http://localhost:3000/clothes', { page, perPage })
       .subscribe((products: Products) => {
         this.products = products.items;
-
-        console.log(products.items);
+        this.totalRecords = products.total;
       });
   }
 
   onProductOutput(product: Product) {
     console.log('Output', product);
+  }
+
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
   }
 }
