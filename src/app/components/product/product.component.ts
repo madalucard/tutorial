d9206103694abf3dd47a5efff120f0fff423aba4
Martin Lapihuska <madalucard@gmail.com>
2024-06-10 17:02:41 +0200

    pipes

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -12,6 +12,8 @@ import { ButtonModule } from 'primeng/button';
 import { ConfirmationService } from 'primeng/api';
 import { ConfirmPopupModule } from 'primeng/confirmpopup';
 import { ToastModule } from 'primeng/toast';
+import { PricePipe } from '../../pipes/price.pipe';
+import { TruncateNamePipe } from '../../pipes/truncate-name.pipe';
 
 @Component({
   selector: 'app-product',
@@ -22,6 +24,8 @@ import { ToastModule } from 'primeng/toast';
     ButtonModule,
     ConfirmPopupModule,
     ToastModule,
+    PricePipe,
+    TruncateNamePipe,
   ],
   providers: [ConfirmationService],
   templateUrl: './product.component.html',
